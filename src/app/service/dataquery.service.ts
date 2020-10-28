7d1049df5a87d52fa1e58e5f683580619ffecc66
jenniferkirwin <lamontagneartist@gmail.com>
2020-10-27 20:22:13 -0400

    Connected to Firestore successfully

---
+++ src/app/service/dataquery.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataqueryService {
+
+  constructor(private firebase: AngularFireFunctions) { }
+
+  callable = this.firebase.httpsCallable('getNotes');
+
+  pullData() {
+    return this.firebase.httpsCallable('getNotes')({ action: "Data Pull" })
+    .subscribe(resp => {
+      console.log( resp.notes );
+      const notesArray = resp.notes;
+      return notesArray;
+    }, err => {
+      console.error( err );
+      return err;
+    });
+  }
+
+}
